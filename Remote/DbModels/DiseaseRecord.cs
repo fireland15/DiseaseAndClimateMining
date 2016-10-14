8d4512aa440ab9d9376d0233bba6225d0aa45572
Ireland <fireland@ewu.edu>
2016-10-14 13:01:52 -0700

    Added stuff

---
+++ Remote/DbModels/DiseaseRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remote.DbModels
+{
+    public class DiseaseRecord
+    {
+        public string Year { get; set; }
+        public string Location { get; set; }
+        public int Week { get; set; }
+        public string DiseaseName { get; set; }
+        public int NewInfections { get; set; }
+    }
+}
